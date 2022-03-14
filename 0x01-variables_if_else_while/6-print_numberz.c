7b28b9eac6ebd2715e45175d91acd706e9671804
GikonyoJohn <99613266+GikonyoJohn@users.noreply.github.com>
2022-03-14 15:40:48 +0300

    base 10 numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * Prints base 10 numbers using putchar
+ *
+ * Description: Prints numbers 0-9
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
