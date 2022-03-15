8bd61ecaa70bc574b591d67528beb39f9b6dc45f
gikonyojohn <gikonyojohn39@gmail.com>
2022-03-15 22:05:10 +0300

    nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * main - Prints Holberton
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints Holberton w/out stdio
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
