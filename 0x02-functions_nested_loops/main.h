ef61e3becd727c9b827d39a816f9d8c6ef4b3cdc
GikonyoJohn <gikonyojohn39@gmail.com>
2022-03-16 14:27:11 +0300

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+/**
+ * Header file for all prototypes
+ * used in 0x02-functions_nested_loops
+ */
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);int add(int, int);
+void print_times_table(int n);
+
+#endif
