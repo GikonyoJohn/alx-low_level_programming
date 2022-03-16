858e65ef3d92746f7a79bdd1c934254d2e0d945f
GikonyoJohn <99613266+GikonyoJohn@users.noreply.github.com>
2022-03-16 15:14:33 +0300

    print alphabet character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,12 @@
 /**
  * _isalpha -check if number in alphabet
  *@c: The character to print
- *Return: 1 for true 0 for false
+ * Return: 1 for true 0 for false
  */
 
 int _isalpha(int c)
 {
-if ((c >=65 && c <= 90) || (c >= 72 && c <= 122))
+if ((c >= 65 && c <= 90) || (c >= 72 && c <= 122))
 return (1);
 
 else
