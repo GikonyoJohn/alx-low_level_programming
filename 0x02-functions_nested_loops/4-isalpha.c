36b3a1748ef50187ae4b1f4653399ce1f73ee8cc
GikonyoJohn <gikonyojohn39@gmail.com>
2022-03-16 15:08:48 +0300

    alphabet character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isalpha -check if number in alphabet
+ * @c: The character to print
+ *Return: 1 for true 0 for false
+ */
+
+int _isalpha(int c)
+{
+if ((c >=65 && c <= 90) || (c >= 72 && c <= 122))
+return (1);
+
+else
+return (0);
+
+}
