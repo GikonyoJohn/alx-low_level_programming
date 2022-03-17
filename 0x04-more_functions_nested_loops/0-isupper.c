889710d114873847ab001786167c8e8de1fb979e
gikonyojohn <gikonyojohn39@gmail.com>
2022-03-17 20:20:04 +0300

    checks uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+
+/**
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Int representing a character
 #include "main.h"
 #include <stdio.h>
 
@@ -8,23 +13,12 @@
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
-	return (0)#include "holberton.h"
-#include <stdio.h>
-
-/**
- * _isupper - Check if paramter c represents an uppercase letter.
- * @c: Int representing a character
- * Return: Always 0.
- */
-int _isupper(int c)
-{
-        if (c >= 65 && c <= 90)
-        {
-                return (1);
-        }
-        return (0)
-
+	else
+	{
+		return (0);
+	}
+}
