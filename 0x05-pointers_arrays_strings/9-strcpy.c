f04ce088058004b2ce0bec08ebab1f4274db0614
gikonyojohn <gikonyojohn39@gmail.com>
2022-03-21 21:28:56 +0300

    copies the string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - copies string to another string
+ * @dest: string pointer
+ * @src: pointer
+ * Return: dest pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
