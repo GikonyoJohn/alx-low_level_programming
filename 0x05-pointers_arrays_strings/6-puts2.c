c3311a499978088bafe6de7adfb4265953d76fdf
gikonyojohn <gikonyojohn39@gmail.com>
2022-03-21 21:08:36 +0300

    print every character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other char of a string
- * @str: type
- *
- */
+ * puts2 - prints every other character of a string
+ * @str: input string to print
+ * Return: always 0
+ **/
 
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i, j;
+
+	i = 0;
+	j = 0;
 
 	while (str[i] != '\0')
 	{
@@ -18,7 +21,7 @@ void puts2(char *str)
 	while (j < i)
 	{
 		_putchar(str[j]);
-		j = j + 2;
+		j += 2;
 	}
 	_putchar('\n');
 }
