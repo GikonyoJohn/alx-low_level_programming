e86fba400979075fe218272d3385aafc40d00af0
John Mwangi <gikonyojohn39@gmail.com>
2022-03-23 14:32:58 +0300

    Concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates  two strings
+ * @dest : string to be overwritten
+ * @src : string to copy from
+ * @n : integer value
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int m;
+	int i;
+
+	m = 0;
+
+	for (i = 0; i < 1000; i++)
+	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		m++;
+	}
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
+	return (dest);
+}
