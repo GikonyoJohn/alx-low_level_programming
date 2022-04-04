cae573f834f197b45ba8f1fd073724dbc437f025
John Mwangi <gikonyojohn39@gmail.com>
2022-04-04 15:55:15 +0300

    concantenates all the arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,20 +1,65 @@
-<<<<<<< HEAD
- k < (len - 1);  k++)
-                {
-                        *(col + k) = *(str++);
-                }
-                *(col + k) = '\0';
-                *(f + j) = col;
-=======
+#include <stdio.h>
 #include "main.h"
-#include <stdlib.h>
->>>>>>> 820cccec0f23d304bf7a0c24e0fde0849df1fba0
 
-                if (j < (count - 1))
-                {
-                        j++;
-                }
-        }
-        *(f + j) = NULL;
-        return (f);
+/**
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}
+
+/**
+ * argstostr - concat
+ * @ac: count
+ * @av: vector
+ * Return: string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k;
+	int len, R = 0;
+	char *p;
+
+	if (!ac || !av)
+	{
+		return (NULL);
+	}
+	R = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		len = _strlen(av[i]) + 1;
+		R += len;
+	}
+	p = malloc(sizeof(char) * R + 1);
+
+	if (!p)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		len = _strlen(av[i]);
+
+		for (j = 0; j < len; j++, k++)
+		{
+			p[k] = av[i][j];
+		}
+		p[k++] = '\n';
+	}
+	p[k] = '\0';
+	return (p);
 }
