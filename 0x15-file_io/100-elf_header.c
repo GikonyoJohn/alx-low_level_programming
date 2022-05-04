dcec73d029a4991ab61412f1a8806e29bf1ad417
GikonyoJohn <gikonyojohn39@gmail.com>
2022-05-04 13:13:32 +0300

    displays content

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -7,7 +7,6 @@
 /**
  * check_elf - Checks if a file is an ELF file.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
- *
  * Description: If the file is not an ELF file - exit code 98.
  */
 void check_elf(unsigned char *e_ident)
