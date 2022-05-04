b42046d8637c2731f558955ad0f0abafcedf64de
GikonyoJohn <gikonyojohn39@gmail.com>
2022-05-04 13:14:38 +0300

    main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,5 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_file(const char *src, const char *dest);
 
+
 #endif
