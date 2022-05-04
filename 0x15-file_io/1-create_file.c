388621c438f5a0a907fe674d91af4ae1b0e66044
GikonyoJohn <gikonyojohn39@gmail.com>
2022-05-04 13:04:29 +0300

    read text file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,6 @@
  * create_file - creates a file and fills it with text
  * @filename: name of the file to create
  * @text_content: text to write in the file
- *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
